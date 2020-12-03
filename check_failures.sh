844d5be60614e9c383aa15e5b0fa92abef032cab
Andrew Anisimov <anisimov.new@gmail.com>
2020-12-03 16:54:15 +0300

    Fix in check_failures script

--- check_failures.sh
+++ check_failures.sh
@@ -3,11 +3,14 @@
 LOG_FILE=$1
 
 # Check for any failure 
-grep -q 'FAIL' ${LOG_FILE} && exit 1
+grep -q 'FAIL' ${LOG_FILE}
+[ ! $? -eq 0 ]  || exit 1
 
 # Check for any error
-grep -q 'ERROR' ${LOG_FILE} && exit 1
+grep -q 'ERROR' ${LOG_FILE}
+[ ! $? -eq 0 ]  || exit 2
 
 # Check for any skipped tests without TODO remark (means reason is unknown for us)
-grep 'SKIP' ${LOG_FILE} | grep -q -v 'TODO' && exit 1
+grep 'SKIP' ${LOG_FILE} | grep -q -v 'TODO'
+[ ! $? -eq 0 ]  || exit 3
 
